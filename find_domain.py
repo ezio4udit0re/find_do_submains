748d00144ba39035729f80233351d1fce9c1b8d1
mrwaterls98 <thuya16k48@gmail.com>
2019-12-09 15:05:14 +0700

    Update find_domain.py

--- find_domain.py
+++ find_domain.py
@@ -10,19 +10,31 @@ Usage: python find_domain.py [OPTIONS]
 [+] Find Domain and Subdmain:
 --ip     [IP ADDRESS]
 [+] Find Subdomain:
---domain 
+--domain    [DOMAIN NAME]
 
 """)
 
 
-def get_sub(string):
-    text = subprocess.Popen(["./findomain-linux", "-t", string], stdout=subprocess.PIPE)
+def get_sub(domain):
+    text = subprocess.Popen(["./findomain-linux", "-t", domain], stdout=subprocess.PIPE)
     pipe = text.communicate()[0].decode()
-    result = re.findall(">>+\s{1}.*", pipe)
-    for v in result:
-        print("\t"+v)
+    result = re.findall(r">>+\s{1}.*", pipe)
+    for s in result:
+        ip = get_ip(s)
+        print(s +" : "+str(ip))
 
-
+def get_ip(url):
+    try:
+        url = url.replace(">> ","")
+        text = subprocess.Popen(["ping", "-c", "1", "{0}".format(url)], stdout=subprocess.PIPE)
+        pipe = text.communicate()[0].decode()
+        result = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", pipe)
+        if not result:
+            return 0
+        else:
+            return result[0]
+    except:
+        print("")
 def load_json(data):
     json1 = json.dumps(data)
     json_d = json.loads(json1)
@@ -30,32 +42,36 @@ def load_json(data):
 
 
 def find_domain(ip):
-    list_domain = []
-    vt_apikey = "252708f32ca7b96d4b10e059bdc561a3a16d8b4b5721c9e7ec75342d887e0f1b"
-    vt_url = "https://www.virustotal.com/vtapi/v2/ip-address/report?apikey=%s&ip=%s"%(vt_apikey, ip)
-    resp = requests.get(vt_url)
-    data = resp.text
-    #xuli_json
-    dic = load_json(data)
-#========== for viewdns_info_api=================
-    # apikey = "c49a6fdcd3462b034e32718b6da368eaa949450e"
-    # url = "https://api.viewdns.info/reverseip/?host=%s&apikey=%s&output=json" % (ip, apikey)
-    # for key, value in dic1:
-    #     if (key == "response"):
-    #         json2 = json.dumps(value)
-    # dic2 = json.loads(json2).items()
-#===========================================
-    for key, value in dic:
-        if (key == "resolutions"):
-            list_domain = value
-    print("Domain Founded: \n\n")
-
-    for i in list_domain:
-        print("[+]"+i['hostname'])
-        get_sub(i['hostname'])
-        print("\n")
-    print("\n\nTotal domains found: %s \n\n" %(len(list_domain)))
-
+    try:
+        list_domain = []
+        vt_apikey = "252708f32ca7b96d4b10e059bdc561a3a16d8b4b5721c9e7ec75342d887e0f1b"
+        vt_url = "https://www.virustotal.com/vtapi/v2/ip-address/report?apikey=%s&ip=%s"%(vt_apikey, ip)
+        resp = requests.get(vt_url)
+        data = resp.text
+        #xuli_json
+        dic = load_json(data)
+    #========== for viewdns_info_api=================
+        # apikey = "c49a6fdcd3462b034e32718b6da368eaa949450e"
+        # url = "https://api.viewdns.info/reverseip/?host=%s&apikey=%s&output=json" % (ip, apikey)
+        # for key, value in dic1:
+        #     if (key == "response"):
+        #         json2 = json.dumps(value)
+        # dic2 = json.loads(json2).items()
+    #===========================================
+        for key, value in dic:
+            if (key == "resolutions"):
+                list_domain = value
+        print("Domain Founded in IP ADDRESS: "+ip+"\n")
+        for i in list_domain:
+            print("[+]"+i['hostname'])
+        print("\n\nTotal domains found: %s \n\n" % (len(list_domain)))
+        print("********Finding subdomain:*********\n")
+        for i in list_domain:
+            print("[+]"+i['hostname'])
+            get_sub(i['hostname'])
+            print("\n")
+    except:
+        print(".")
 
 if __name__ == "__main__":
    if len(sys.argv) <= 2:
